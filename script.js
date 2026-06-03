0b5911f493bb740d2efa387eb47cb7b8ec2151d2
Amanda-gu <guoj826@newschool.edu>
2026-06-03 16:18:55 -0400

    new content

--- script.js
+++ script.js
@@ -20,39 +20,3 @@ window.addEventListener('pointermove', (e) => {
         image.style.top = `${e.clientY}px`
     })
 });
-// let canvas = document.getElementById('pointer-box');
-//   	// let hLine = document.getElementById('h-line')
-//     // let vLine = document.getElementById('v-line')
-//     // let sq = document.getElementById('sq')
-
-
-// window.addEventListener('pointermove', (e) => {
-//     canvas.width = window.innerWidth;
-//     canvas.height = window.innerHeight;
-// //previous solution works well on desktop but on mobile, the pointer move event colides with the scroll
-// //using canvas to draw the shapes instead of html elements, so it works better for mobile.
-//     let ctx = canvas.getContext("2d");
-//     ctx.fillStyle = "black";
-//     ctx.lineWidth = 0.5;
-//     // Add a rectangle at (10, 10) with size 100x100 pixels
-//     ctx.fillRect(`${e.pageX}`, `${e.pageY}`, 10, 10);
-
-//     ctx.moveTo(`${e.pageX}`, `${e.pageY}`);
-//     // ctx.lineTo(0, 0);
-//     ctx.lineTo(0, `${e.pageY}`);
-//     ctx.lineTo(`${e.pageX}`, 0);
-//     // ctx.lineTo(`${e.pageX}`, `${e.pageY}`);
-//     // ctx.lineTo(0,  `${e.pageY*2}`);
-//     // ctx.lineTo(`${e.pageX*2}`, 0);
-//     // ctx.moveTo(`${e.pageX* 2}`, `${e.pageY}`);
-//     // ctx.lineTo(`${e.pageX}`, `${e.pageY * 2}`);
-//     ctx.stroke();
-//     // hLine.style.left = `${e.pageX}px`
-//     // hLine.style.top = `${e.pageY}px`
-
-//     // vLine.style.left = `${e.pageX}px`
-//     // vLine.style.top = `${e.pageY}px`
-
-//     // sq.style.left = `${e.pageX}px`
-//     // sq.style.top = `${e.pageY}px`
-// });
